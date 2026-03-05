cdf34edb57bc4fa32611be4d31f00ee81de7402b
niltal-amal <tataamal1128@gmail.com>
2026-03-06 06:07:22 +0700

    merge report and update feature

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+// 1. Wajib tambahkan ini agar Axios mengirimkan cookie session kembali ke server
+window.axios.defaults.withCredentials = true;
+
+// 2. Wajib tangkap token CSRF dari meta tag HTML dan masukkan ke header Axios
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common["X-CSRF-TOKEN"] = token.content;
+} else {
+    console.error(
+        "CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token",
+    );
+}
